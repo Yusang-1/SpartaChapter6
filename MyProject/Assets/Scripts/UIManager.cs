c6ea6a419a38d53397063ef91efb9f44b2186186
Yusang-1 <tearless25hg@gmail.com>
2025-05-20 20:26:35 +0900

    add : UI에 대한 코드

---
+++ MyProject/Assets/Scripts/UIManager.cs
@@ -0,0 +1,33 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    public static UIManager Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                return null;
+            }
+            return instance;
+        }
+    }
+
+
+}
