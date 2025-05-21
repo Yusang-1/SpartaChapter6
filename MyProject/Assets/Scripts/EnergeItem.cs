cb51c25bb1426e573b6a0db1e7a22635e3c285ed
Yusang-1 <tearless25hg@gmail.com>
2025-05-21 15:55:03 +0900

    feat : 코루틴 기능 추가

--- MyProject/Assets/Scripts/EnergeItem.cs
+++ MyProject/Assets/Scripts/EnergeItem.cs
@@ -1,18 +1,28 @@
+using System.Collections;
+using System.Threading;
 using UnityEngine;
 
 public class EnergeItem : MonoBehaviour, InterfaceStats
-{        
+{
+    [SerializeField] Material material;
+
+    void Start()
+    {
+        
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
-            MomentaryHeal(ref StatsManager.Instance.playerStats.curStamina, 20.0f);
+        if (other.gameObject.tag == "Player")
+        {
+            material.color = new Color(material.color.r, material.color.g, material.color.b, 0);            
+            StartCoroutine(cHeal());
+        }
 
-        LimitValue(ref StatsManager.Instance.playerStats.curStamina, StatsManager.Instance.playerStats.MaxStamina);
-        Destroy(this.gameObject);
     }
 
     public void ContinuousHeal(ref float energe, float healRate)
     {
+
         energe += healRate;
     }
 
@@ -23,9 +33,20 @@ public class EnergeItem : MonoBehaviour, InterfaceStats
 
     public void LimitValue(ref float energe, float max)
     {
-        if(energe > max)
+        if (energe > max)
         {
             energe = max;
         }
+    }
+
+    public IEnumerator cHeal()
+    {        
+        for (float g = 0f; g < 600; g++)
+        {
+            ContinuousHeal(ref StatsManager.Instance.playerStats.curStamina, 0.2f);
+            LimitValue(ref StatsManager.Instance.playerStats.curStamina, StatsManager.Instance.playerStats.MaxStamina);
+            yield return null;
+        }
+        Destroy(this.gameObject);
     }
 }
