4cd7079fcc738734dc0b580071e38fcc16c33d15
Yusang-1 <tearless25hg@gmail.com>
2025-05-21 19:30:07 +0900

    feat : 상호작용 UI 띄우는 기능 추가

---
+++ MyProject/Assets/Scripts/Interaction.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting;
+
+public class Interaction : MonoBehaviour
+{
+    [SerializeField] float maxHitDistance;
+    [SerializeField] LayerMask mask;
+    [SerializeField] GameObject InteractUI;
+    [SerializeField] TextMeshProUGUI InteractText;
+    float searchRate = 0.5f;
+    float lastSearchTime = 0;
+    GameObject curInteractGameObject;
+    Camera cam;
+    InteractableObject interactableObject;
+    public void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void FixedUpdate()
+    {       
+        if(Time.time - lastSearchTime > searchRate)
+        {
+            lastSearchTime = Time.time;
+            Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, maxHitDistance, mask) == true)
+            {
+                if(hit.collider.gameObject != curInteractGameObject)
+                {
+                    curInteractGameObject = hit.collider.gameObject;
+                    interactableObject = hit.collider.GetComponent<InteractableObject>();
+                    ShowUI();
+                }
+            }
+            else
+            {
+                curInteractGameObject = null;
+                InteractUI.SetActive(false);
+            }
+        }
+    }
+
+    void ShowUI()
+    {
+        InteractUI.SetActive(true);
+        InteractText.text = interactableObject.getObjectData();
+    }
+}
