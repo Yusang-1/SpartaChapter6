5028ce557258262458fa7e073f80b1a29ded6a79
Yusang-1 <tearless25hg@gmail.com>
2025-05-21 16:43:07 +0900

    feat : 아이템 코루틴 기능 추가
    - 아이템 섭취시 해당하는 스텟의 UI가 깜빡이는 기능

--- MyProject/Assets/Scripts/EnergeItem.cs
+++ MyProject/Assets/Scripts/EnergeItem.cs
@@ -5,21 +5,17 @@ using UnityEngine;
 public class EnergeItem : MonoBehaviour, InterfaceStats
 {
     [SerializeField] Material material;
+    [SerializeField] StatUI statUI;
 
-    void Start()
-    {
-        
-    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             material.color = new Color(material.color.r, material.color.g, material.color.b, 0);            
             StartCoroutine(cHeal());
+            statUI.UIBlink(10, 2);
         }
-
     }
-
     public void ContinuousHeal(ref float energe, float healRate)
     {
 
@@ -40,13 +36,14 @@ public class EnergeItem : MonoBehaviour, InterfaceStats
     }
 
     public IEnumerator cHeal()
-    {        
+    {
         for (float g = 0f; g < 600; g++)
         {
             ContinuousHeal(ref StatsManager.Instance.playerStats.curStamina, 0.2f);
             LimitValue(ref StatsManager.Instance.playerStats.curStamina, StatsManager.Instance.playerStats.MaxStamina);
             yield return null;
         }
+        statUI.UIBlinkStop();
         Destroy(this.gameObject);
     }
 }
--- MyProject/Assets/Scripts/StatUI.cs
+++ MyProject/Assets/Scripts/StatUI.cs
@@ -1,17 +1,63 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StatUI : MonoBehaviour
 {
     [SerializeField] RectTransform rectTransformHP;
     [SerializeField] RectTransform rectTransformStamina;
+    [SerializeField] Image imageHP;
+    private Color imageHPColor;
+    [SerializeField] Image imageStamina;
+    private Color imageStaminaColor;
+    Coroutine coroutine;
     void Start()
     {
         rectTransformHP = GameObject.Find("HP").GetComponent<RectTransform>();
         rectTransformStamina = GameObject.Find("Stamina").GetComponent<RectTransform>();
+        imageHP = GameObject.Find("HP").GetComponent<Image>();
+        imageStamina = GameObject.Find("Stamina").GetComponent<Image>();
+        imageHPColor = imageHP.color;
+        imageStaminaColor = imageStamina.color;
     }
 
     void Update()
     {
         rectTransformStamina.localScale = new Vector3(StatsManager.Instance.playerStats.curStamina / StatsManager.Instance.playerStats.MaxStamina,1,1);
+        rectTransformHP.localScale = new Vector3(StatsManager.Instance.playerStats.curHp / StatsManager.Instance.playerStats.MaxHp, 1, 1);
+    }
+
+    public void UIBlink(int time, int type)
+    {
+        coroutine = StartCoroutine(UIBlinkCouroutine(time,type));
+    }
+    public void UIBlinkStop()
+    {
+        StopCoroutine(coroutine);
+        imageHP.color = imageHPColor;
+        imageStamina.color= imageStaminaColor;
+    }
+    public IEnumerator UIBlinkCouroutine(int time, int type)
+    {
+        Image image = type == 1 ? imageHP : imageStamina;
+        for (int i = 0; i < time*60; i++)
+        {
+            for(float f = 1f; f > 0.5f; f -= 0.01f)
+            {
+                Color c = image.color;
+                c.a = f;
+                image.color = c;
+                yield return null;
+            }
+            yield return new WaitForSecondsRealtime(0.02f);
+
+            for (float f = 1f; f > 0.5f; f -= 0.01f)
+            {
+                Color c = image.color;
+                c.a = f;
+                image.color = c;
+                yield return null;
+            }
+        }
     }
 }
